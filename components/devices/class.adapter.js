d0731d18c2ee5e79aad0a58ef26824f894f149a3
mStirner <marc.stirner@hotmail.de>
2022-04-23 23:51:40 +0200

    example added

--- components/devices/class.adapter.js
+++ components/devices/class.adapter.js
@@ -12,9 +12,35 @@ const { Duplex } = require("stream");
  * @class Adapter 
  * @extends Duplex https://nodejs.org/dist/latest-v16.x/docs/api/stream.html#class-streamduplex
  * 
- * @param {Array} stack Gute frage....
+ * @param {Array} stack Array of called adapter functions
  * @param {Object} upstream Upstream socket connecto to device (WebSocket)
  * @param {Object} options Duplex stream options
+ * 
+ * @example
+ * ```js
+ * const Adapter = require(".../components/devices/class.adapter.js");
+ * 
+ * let stack = ["raw"].map((name) => {
+ *     try {
+ *
+ *         return require(path.resolve(process.cwd(), "adapter", `${name}.js`))();
+ *
+ *     } catch (err) {
+ *
+ *         console.error(`Error in adapter "${name}" `, err);
+ *
+ *     }
+ * });
+ * 
+ * let adapter = new Adapter(stack, upstream, {
+ *   encoding: "utf8"
+ * });
+ * 
+ * adapter.write("foo");
+ * ```
+ * 
+ * @see Quick-start backend/quick-start?id=dataflow
+ * @see InterfaceStream backend/components/devices/class.interfaceStream.js
  */
 module.exports = class Adapter extends Duplex.from {
     constructor(stack, upstream, options) {
