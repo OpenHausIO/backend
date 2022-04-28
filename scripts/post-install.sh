a857a04ba4ce224a5a81e2c4cd3b0bbda4661e25
mStirner <marc.stirner@hotmail.de>
2022-04-28 21:13:08 +0200

    comment added

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -3,6 +3,8 @@
 pwd=$(pwd);
 
 #echo $pwd/scripts/hooks/;
+# rm -rf $pwd/.git/hooks
+# https://rjzaworski.com/2018/01/keeping-git-hooks-in-sync
 if [[ ! -d $pwd/.git/hooks ]]
 then
     ln -s $pwd/scripts/hooks/ $pwd/.git/
