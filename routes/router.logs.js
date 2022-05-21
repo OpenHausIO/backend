6dff11cf81ef01c15051409f01431ddb5944475b
mStirner <marc.stirner@hotmail.de>
2022-05-21 17:53:33 +0200

    WebSocket stream added, fix #157

--- routes/router.logs.js
+++ routes/router.logs.js
@@ -1,8 +1,110 @@
+// internal modules
 const fs = require("fs");
 const readline = require("readline");
+const { Readable } = require("stream");
+
+// external modules
+const WebSocket = require("ws");
 
 module.exports = (app, router) => {
+
+    // websocket server
+    let wss = new WebSocket.Server({
+        noServer: true
+    });
+
+    // detect broken connections
+    let interval = setInterval(() => {
+        wss.clients.forEach((ws) => {
+
+            if (!ws.isAlive) {
+                ws.terminate();
+                return;
+            }
+
+            ws.isAlive = false;
+            ws.ping();
+
+        });
+    }, Number(process.env.API_WEBSOCKET_TIMEOUT));
+
+
+    // if the server closes
+    // clear the interval
+    wss.on("close", () => {
+        clearInterval(interval);
+    });
+
+
+    // http route handler
     router.get("/", (req, res) => {
+
+        // logpath to combined logfile
+        let logfile = `${process.env.LOG_PATH}/combined.log`;
+
+        // readable stream for readline module
+        let readable = new Readable({
+            read() { }
+        });
+
+        // open the logfile and watch for changes
+        // push changes into readable stream
+        fs.open(logfile, (err, fd) => {
+            if (err) {
+                console.error(err);
+                process.exit();
+            } else {
+
+                let position = 0;
+
+                // initial read
+                // set offset bytes
+                fs.read(fd, {
+                    position
+                }, (err, bytesRead, buffer) => {
+                    if (err) {
+
+                        console.erro(err);
+                        process.exit();
+
+                    } else {
+
+                        position += bytesRead;
+                        readable.push(buffer.slice(0, bytesRead));
+
+                    }
+                });
+
+                // listen for file changes
+                // push to readable stream 
+                // add readed bytes to offset
+                fs.watch(logfile, (event) => {
+                    if (event === "change") {
+
+                        fs.read(fd, {
+                            position
+                        }, (err, bytesRead, buffer) => {
+                            if (err) {
+
+                                console.erro(err);
+                                process.exit(1);
+
+                            } else {
+
+                                position += bytesRead;
+                                readable.push(buffer.slice(0, bytesRead));
+
+                            }
+                        });
+
+                    }
+                });
+
+            }
+        });
+
+        // check if connection is a simple get request and return logfile lines
+        // or handle the connection as websocket and keep push new log messages?
         if ((!req.headers["upgrade"] || !req.headers["connection"])) {
 
             let {
@@ -12,10 +114,9 @@ module.exports = (app, router) => {
 
             let output = [];
             let lines = 0;
-            let stream = fs.createReadStream(`${process.env.LOG_PATH}/combined.log`);
 
             let rl = readline.createInterface({
-                input: stream
+                input: readable
             });
 
             rl.on("line", (line) => {
@@ -40,11 +141,49 @@ module.exports = (app, router) => {
 
         } else {
 
-            // handle websocket
-            // TODO implement websocket stream
-            // watch for changes in combined logfile and send events over ws
-            res.status(501).end();
+            // listen for websockt clients
+            // keep sending new log entrys to client
+            wss.once("connection", (ws) => {
+
+                readable.on("end", () => {
+                    ws.close();
+                });
+
+                let rl = readline.createInterface({
+                    input: readable
+                });
+
+                rl.on("line", (line) => {
+                    ws.send(String(line));
+                });
+
+                rl.on("close", () => {
+                    ws.close();
+                });
+
+                ws.on("close", () => {
+                    rl.close();
+                    readable.destroy();
+                });
+
+            });
+
+            // handle request as websocket
+            // perform websocket handshake 
+            wss.handleUpgrade(req, req.socket, req.headers, (ws) => {
+
+                ws.isAlive = true;
+
+                ws.on("pong", () => {
+                    ws.isAlive = true;
+                });
+
+                wss.emit("connection", ws, req);
+
+            });
 
         }
+
     });
+
 };
