350a3e15c3dcaab3b99acfeeeeea1b43bf875c38
mStirner <marc.stirner@hotmail.de>
2022-10-25 19:27:33 +0200

    added

---
+++ components/store/class.store.js
@@ -0,0 +1,100 @@
+const { EventEmitter } = require("events");
+const Value = require("./class.value.js");
+
+
+/**
+ * @description
+ * Represents a single key/value item.
+ * 
+ * @class Value
+ * 
+ * @param {Object} obj Object that matches the item schema. See properties below:
+ * 
+ * @property {String} _id MongoDB Object id is as string
+ * @property {String} key Object key, like in a regular object
+ * @property {String|Boolean|Numver} value Value of the property <key>
+ * @property {String} type Type of <value> key: "string", "boolean" or "number"
+ * @property {String} description Description for what the key is used
+ * @property {String} namespace Object namespace, `uuid -v4`
+ * @property {String} item MongoDB ObjectID for for what item the config applays (E.g: device or endpoint item)
+ */
+class Store {
+
+    #privates = new Map();
+
+    constructor(obj, scope) {
+
+        // create event emitter for lean object
+        let events = new EventEmitter();
+        this.#privates.set("events", events);
+
+        Object.assign(this, obj);
+        this._id = String(obj._id);
+
+        this.config = obj.config.map((data) => {
+            return new Value(data, async () => {
+                try {
+
+                    // feedback
+                    scope.logger.debug(`Value in store "${this._id}" changed: ${data.key}=${data.value}`);
+
+                    // update item in database
+                    await scope.update(this._id, this);
+
+                } catch (err) {
+
+                    scope.logger.warn(err, `Could not update item value in database. (${obj._id}) ${data.key}=${data.value}`);
+
+                } finally {
+
+                    // notify for changes
+                    events.emit("changed", data.key, data.value);
+
+                }
+            });
+        });
+
+    }
+
+    changes() {
+        return this.#privates.get("events");
+    }
+
+    lean() {
+
+        /*
+        // get private event emitter
+        let events = this.#privates.get("events");
+
+        // create lean object with getter/setter from value class
+        let obj = this.config.reduce((acc, cur, i, arr) => {
+
+            // get property descriptor to define getter/setter functions
+            let descriptor = Object.getOwnPropertyDescriptor(arr[i], "value");
+
+            // define original property descriptor
+            Object.defineProperty(acc, cur.key, descriptor);
+
+            return acc;
+
+        }, {});
+
+        // "hide" events behind lean object
+        return _mixins([obj, events], {
+            setPrototype: true
+        });
+        */
+
+        return this.config.reduce((prev, cur) => {
+
+            let descriptor = Object.getOwnPropertyDescriptor(cur, "value");
+            Object.defineProperty(prev, cur.key, descriptor);
+
+            return prev;
+
+        }, {});
+    }
+
+}
+
+module.exports = Store;
