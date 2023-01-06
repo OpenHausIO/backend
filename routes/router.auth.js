264122f3873cc260619e17817f62f2614bebbd64
mStirner <marc.stirner@hotmail.de>
2023-01-06 20:23:57 +0100

    added route for client to check if auth is needed

--- routes/router.auth.js
+++ routes/router.auth.js
@@ -3,6 +3,24 @@ const C_USERS = require("../components/users");
 
 module.exports = (app, router) => {
 
+    router.get("/", (req, res) => {
+
+        console.log("/auth request from:", req.ip);
+
+        if (process.env.API_AUTH_ENABLED === "true") {
+
+            // tell the client it needs to authenticate
+            res.status(401).end();
+
+        } else {
+
+            // its ok, just start with api requests
+            res.status(200).end();
+
+        }
+
+    });
+
     router.post("/login", (req, res) => {
         C_USERS.login(req.body.email, req.body.password, (err, user) => {
             if (err) {
