77da14e649432987143fd349960cf132274096a0
Yuvraj Singh Sisodiya <yuvrajsisodiya7302@gmail.com>
2023-04-12 17:07:04 +0530

    Update db_auth.js

--- system/db_auth.js
+++ system/db_auth.js
@@ -1,32 +1,20 @@
 const { MongoClient } = require('mongodb');
 
-async function checkAuth(username) {
-    const databaseName = 'mydatabase';
-    const uri = `mongodb://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@localhost:27017/OpenHaus`;
-    const client = new MongoClient(uri, { useNewUrlParser: true });
+const url = 'mongodb://<user>:<pass>:27017/OpenHaus';
+// process.env.DATABASE_URL
 
-    try {
-        await client.connect();
-        console.log('Connected to MongoDB server');
+const client = new MongoClient(url);
 
-        // Query the database for the specified username
-        const db = client.db();
-        const users = db.collection('users');
-        const user = await users.findOne({ username });
-
-        // Check if the user exists and has a password
-        if (user && user.password) {
-            return true;
-        } else {
-            return false;
-        }
-    } catch (err) {
-        console.error(err);
-        return false;
-    } finally {
-        await client.close();
-    }
+async function checkAuth() {
+  try {
+    await client.connect();
+    // console.log('Connected to MongoDB server');
+    // Perform database operations
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.close();
+  }
 }
 
 module.exports = checkAuth;
-// Example usage: checkAuth('myusername')
