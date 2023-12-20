a5fd58b72cca6302079ae0d7d97227859bdc6c8a
Marc Stirner <marc.stirner@it2media.de>
2023-12-20 14:38:27 +0100

    add http test, fix #370

---
+++ tests/http-api/index.js
@@ -0,0 +1,84 @@
+const { describe, it } = require("mocha");
+const newman = require("newman");
+const { fork } = require("child_process");
+const path = require("path");
+const crypto = require("crypto");
+const assert = require("assert");
+
+const collection = require("../../postman.json");
+
+
+describe("HTTP API", function () {
+
+    this.timeout(10000);
+
+    //let HTTP_PORT = crypto.randomInt(2048, 1024);
+    let child = null;
+
+    it("- Should start OpenHaus", (done) => {
+
+        child = fork(path.resolve(process.cwd(), "index.js"), {
+            silent: true,
+            env: Object.assign({}, process.env, {
+                UUID: crypto.randomUUID(),
+                DATABASE_NAME: "/test",
+                VAULT_MASTER_PASSWORD: crypto.randomBytes(24).toString("hex"),
+                USERS_JWT_SECRET: crypto.randomBytes(24).toString("hex")
+            })
+        });
+
+        child.on("spawn", () => {
+            setTimeout(done, 1500);
+        });
+
+        child.on("error", (err) => {
+            done(err);
+        });
+
+    });
+
+
+    // https://gist.github.com/davfive/eae043135ed98b9647ad631bbfc1ab38
+    it("- Should not have any items in summary.run.failres array", (done) => {
+
+        let emitter = newman.run({
+            collection,
+            reporters: "json",
+            workingDir: __dirname
+        });
+
+        emitter.once("done", (err, summary) => {
+            if (err || summary.error) {
+
+                // stop backend
+                child.kill();
+
+                done(err || summary.error);
+
+            } else {
+
+                // stop backend
+                child.kill();
+
+                summary.run.failures.forEach(({ source: { request }, error }) => {
+                    console.error(`[${request.method}] ${request.url.toString()}`, error.message);
+                });
+
+                assert.equal(summary.run.failures.length, 0);
+
+                // give some time to kill the backend
+                setTimeout(done, 1000);
+                //done();
+
+            }
+        });
+
+    });
+
+
+    it("- Should stop the backend", () => {
+        assert(!child.connected);
+    });
+
+
+});
