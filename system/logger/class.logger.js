18214daefb2f3e4a22cbc05a236d501572047841
Marc Stirner <marc.stirner@hotmail.de>
2024-05-03 17:39:07 +0200

    remove 'console.log' & added "consumer" check for stream

--- system/logger/class.logger.js
+++ system/logger/class.logger.js
@@ -6,14 +6,9 @@ const { Transform } = require("stream");
 const exporter = new Transform({
     transform(chunk, encoding, cb) {
 
-        console.log("exporter stream is piped:", this._readableState.pipes.length > 0);
-
-        // ignore & drop every message if no pipe/consumer
-        // otherwise this jams up the stream
-        // TODO: check if a event listener is used to consume data
-        // try with: this.listenerCount('data'|'readable')
-        // if event listener is used, this approach drops the chunk
-        if (this._readableState.pipes.length > 0) {
+        // ignore & drop every message if no pipe/consumer. Otherwise this jams up the stream
+        // TODO: check with this.listenerCount('data'|'readable')?
+        if (this._readableState.pipes.length > 0 || this._events.data instanceof Function) {
             this.push(chunk);
         }
 
