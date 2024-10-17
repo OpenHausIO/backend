16a17304280af5f84252c5c88f030b9c600acb4b
Marc Stirner <marc.stirner@hotmail.de>
2024-10-17 14:37:18 +0200

    comment added

--- components/scenes/makro-types.js
+++ components/scenes/makro-types.js
@@ -33,10 +33,16 @@ module.exports = {
                     // how should this be catched?
                     // reject the makro execution if one command fails?
                     // or ignore it simple, and continue?
+                    //resolve(_id);
 
                 }]
             });
 
+            // this does not wait for the prveious command to execute
+            // execute next makro in scenes, see #505
+            // pro: "parallel" execution of commands
+            // contra: timestamp started/finished does not match. e.g.:
+            // `.finished` is set before the last command is executed
             resolve(_id);
 
         });
